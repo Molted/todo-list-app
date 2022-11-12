aac90748d5d988391917257de9409c7949a8a58a
Molted <joshuaconcepcion12.jc@gmail.com>
2022-11-12 21:59:37 +0800

    Simple CRUD (API)

---
+++ app/Http/Controllers/API/TodoController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TodoRequest;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::all();
+        return response()->json([
+            'status' => true,
+            'todo' => $todos
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TodoRequest $request)
+    {
+       $todo = Todo::create($request->all());
+
+       return response()->json([
+            'status' => true,
+            'message' => "Todo created successfully.",
+            'todo' => $todo
+       ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return response()->json([
+            'status' => true,
+            'todo' => $todo
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(TodoRequest $request, Todo $todo)
+    {
+        $todo->update($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => "Todo updated successfully.",
+            'todo' => $todo
+       ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => "Todo deleted successfully.",
+       ], 200);
+    }
+}
